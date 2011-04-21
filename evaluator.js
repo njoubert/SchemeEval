1d50eb64e61c1c0246d969c2b678ec482baa9f7a
Niels Joubert <njoubert@gmail.com>
2011-04-21 09:05:23 -0700

    has a shitty applying simple procedures.

--- evaluator.js
+++ evaluator.js
@@ -26,10 +26,8 @@ var Evaluator = function() {
   obj.env = new Environment(null);
   
   obj.eval = function(expr, env) {
-
-    //primitive
     if (isEmpty(expr)) {
-      return false;
+      return;
     } else if (isNumber(expr)) {
       return parseFloat(expr);
     } else if (isString(expr)) {
@@ -40,7 +38,7 @@ var Evaluator = function() {
       return obj.eval_assignment(expr, env);
     } else if (isApply(expr)) {
       var first = expr.shift();
-      return obj.apply(first, expr)
+      return obj.apply(first, expr, env)
     } else {
       return "Unknown operation";
     }
@@ -53,9 +51,29 @@ var Evaluator = function() {
     return value;
   }
 
-  obj.apply = function(procedure, exprs) {
+  obj.apply = function(procedure, exprs, env) {
+    if (isSimpleProc(procedure)) {
+      var args = exprs.map(function(e) { return obj.eval(e, env); });
+      return obj.applySimple(procedure, args);
+    }
       return "Applying " + procedure + " to " + exprs;
   };
+  
+  obj.applySimple = function(procedure,args) {
+    if (procedure == "+") {
+      return args.reduce(function(a,b) {return a+b;});
+    }
+    if (procedure == "-") {
+      return args.reduce(function(a,b) {return a-b;});
+    }
+    if (procedure == "*") {
+      return args.reduce(function(a,b) {return a*b;});
+    }
+    if (procedure == "/") {
+      return args.reduce(function(a,b) {return a/b;});
+    }
+
+  }
 
 
   function isEmpty(expr) {
@@ -75,6 +93,12 @@ var Evaluator = function() {
   }
   function isApply(expr) {
     return (is_list(expr));
+  }
+  function isSimpleProc(procedure) {
+    return (procedure === "+" ||
+            procedure === "-" ||
+            procedure === "*" ||
+            procedure === "/");
   }
 
   function first(str) {
@@ -213,6 +237,70 @@ window.onload = function() {
   evaluator = Evaluator();
   console = Console('console01', evaluator);
 };
+
+if (!Array.prototype.map)
+{
+  Array.prototype.map = function(fun /*, thisp*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = new Array(len);
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this)
+        res[i] = fun.call(thisp, this[i], i, this);
+    }
+
+    return res;
+  };
+}
+
+if (!Array.prototype.reduce)
+{
+  Array.prototype.reduce = function(fun /*, initial*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    // no value to return if no initial value and an empty array
+    if (len == 0 && arguments.length == 1)
+      throw new TypeError();
+
+    var i = 0;
+    if (arguments.length >= 2)
+    {
+      var rv = arguments[1];
+    }
+    else
+    {
+      do
+      {
+        if (i in this)
+        {
+          rv = this[i++];
+          break;
+        }
+
+        // if array contains no values, no initial value to return
+        if (++i >= len)
+          throw new TypeError();
+      }
+      while (true);
+    }
+
+    for (; i < len; i++)
+    {
+      if (i in this)
+        rv = fun.call(null, rv, this[i], i, this);
+    }
+
+    return rv;
+  };
+}
 
 })();
 
